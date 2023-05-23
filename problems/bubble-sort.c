f1961ec9e80b0e8412f22f8647eab22692fd0632
Satyaki Dey <sdey2836@gmail.com>
2023-05-24 03:07:04 +0530

    added programs

--- problems/bubble-sort.c
+++ problems/bubble-sort.c
@@ -20,15 +20,15 @@ void main()
     }
     int temp;
 
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 1; i < n; i++)
     {
-        for (int j = 0; j < n - i - 1; j++)
+        for (int j = 0; j < n - i; j++)
         {
             if (a[j] > a[j + 1])
             {
-                int temp = a[j + 1];
-                a[j + 1] = a[j];
-                a[j] = temp;
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
             }
         }
     }
