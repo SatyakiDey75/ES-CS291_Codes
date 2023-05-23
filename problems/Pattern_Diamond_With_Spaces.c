17b3d36c81b4d8268d5cea50f5f5f9368e9258be
Srinjoy Sen Chowdhury <116475469+AllMightLegend@users.noreply.github.com>
2023-05-23 22:09:42 +0530

    Create Pattern_Diamond_With_Spaces.c

---
+++ problems/Pattern_Diamond_With_Spaces.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+//for rows=7
+/*    ********
+      ***  ***
+      **    **
+      *      *
+      **    **
+      ***  ***
+      ********   */
+int main() {
+    int rows,i,j,k,l;
+    printf("Enter rows : ");
+    scanf("%d",&rows);
+    for(i=(rows/2)+1;i>=1;i--){
+        for(j=i;j>=1;j--){
+            printf("*");
+        }
+        for(k=((rows/2)-i+1)*2;k>0;k--){
+            printf(" ");
+        }
+        for(l=i;l>=1;l--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=2;i<=(rows/2)+1;i++){
+        for(j=i;j>=1;j--){
+            printf("*");
+        }
+        for(k=((rows/2)-i+1)*2;k>0;k--){
+            printf(" ");
+        }
+        for(l=i;l>=1;l--){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
